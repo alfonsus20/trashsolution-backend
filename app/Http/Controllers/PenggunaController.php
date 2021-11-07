839b566b0ce814712ffc76f8265208c4183f635a
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-11-08 00:11:22 +0700

    add broadcast driver

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PenggunaController extends Controller
 {
-    public function index(){
-        return response()->json(['message'=> 'pengguna index']);
-    }
+    // public function index(){
+    //     return response()->json(['message'=> 'pengguna index']);
+    // }
 }
