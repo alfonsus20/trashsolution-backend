ad9b6f8bb0f4869938b4f73054299e84573adffe
unknown <faizaulia727@gmail.com>
2021-11-11 10:49:23 +0700

    complete data daftar sampah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -103,5 +103,61 @@ class DatabaseSeeder extends Seeder
             'harga' => 1000,
             'gambar' => ''
         ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Karton/kardus 100g',
+            'jenis' => 'Kertas',
+            'ukuran' => '100g',
+            'harga' => 150,
+            'gambar' => ''
+        ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Karton/kardus 500g',
+            'jenis' => 'Kertas',
+            'ukuran' => '500g',
+            'harga' => 750,
+            'gambar' => ''
+        ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Karton/kardus 1kg',
+            'jenis' => 'Kertas',
+            'ukuran' => '1kg',
+            'harga' => 1500,
+            'gambar' => ''
+        ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Kaleng alumunium <200g',
+            'jenis' => 'Logam',
+            'ukuran' => '<200ml',
+            'harga' => 150,
+            'gambar' => ''
+        ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Kaleng alumunium 200-500ml',
+            'jenis' => 'Logam',
+            'ukuran' => '200-500ml',
+            'harga' => 200,
+            'gambar' => ''
+        ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Kaleng alumunium 500-800ml',
+            'jenis' => 'Logam',
+            'ukuran' => '500-800ml',
+            'harga' => 300,
+            'gambar' => ''
+        ]);
+
+        DB::table('sampah')->insert([
+            'nama' => 'Kaleng alumunium 800-1000ml',
+            'jenis' => 'Logam',
+            'ukuran' => '800-1000ml',
+            'harga' => 500,
+            'gambar' => ''
+        ]);
     }
 }
